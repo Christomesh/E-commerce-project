fe42a7990527d31667dee91e0d0c30e47d9d4263
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-25 15:03:08 +0100

    updated app.js

--- app.js
+++ app.js
@@ -6,18 +6,41 @@ const express = require("express")
 const app = express()
 
 // Other packages
+const morgan = require("morgan")
+
+// database
+const connectDB = require("./db/connect")
+
+// routers
+const authRouter = require("./routes/authRoutes")
+
+//middleware
+const notFound = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
+
+app.use(morgan('tiny'))
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send("E-commerce project")
+})
+app.use('/api/v1/auth', authRouter)
 
 
 
 
-// spin the server
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
 const port = process.env.PORT || 5000
-
+// spin the server
 const start = async () =>{
     try {
-        app.listen(port, ()=>{
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, ()=>
             console.log(`Server listening on port ${port}`)
-        })
+        )
     } catch (error) {
         console.log(error)
     }
