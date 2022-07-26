92cefe14283f585792a3bebf242107afc3429977
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-26 18:37:35 +0100

    Implemented rigister, login and logout route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
 const {StatusCodes} = require("http-status-codes");
+const bcrypt = require("bcryptjs")
 const CustomError = require("../errors");
+const {attachCookiesToResponse, createTokenUser} = require("../utils")
 
 const register = async(req, res) =>{
     const {email, name, password} = req.body;
@@ -16,15 +18,40 @@ const register = async(req, res) =>{
 
     const user = await User.create({email, name, password, role});
 
-    res.status(StatusCodes.CREATED).json({user});
+    const tokenUser = createTokenUser(user);
+
+    attachCookiesToResponse({res, user: tokenUser})
+  
+    res.status(StatusCodes.CREATED).json({user: tokenUser});
 };
 
-const login = async(req, res) =>{
-    res.send("login")
-};
+const login = async (req, res) => {
+    const { email, password } = req.body;
+  
+    if (!email || !password) {
+      throw new CustomError.BadRequestError('Please provide email and password');
+    }
+    const user = await User.findOne({ email });
+  
+    if (!user) {
+      throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    }
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+      throw new CustomError.UnauthenticatedError('Invalid Credentials');
+    }
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({ res, user: tokenUser });
+  
+    res.status(StatusCodes.OK).json({ user: tokenUser });
+  };
 
 const logout = async(req, res) =>{
-    res.send("logout")
+    res.cookie('token', 'logout', {
+        httpOnly: true,
+        expires: new Date(Date.now()+ 1000)
+    });
+    res.status(StatusCodes.Ok).json({ msg: "user logged out"}) 
 };
 
 module.exports = {
