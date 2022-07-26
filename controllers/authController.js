e161f9617aad2f50c7f67f24818bf8995e1caf3d
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-26 11:02:20 +0100

    registered an admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,34 @@
+const User = require("../models/User");
+const {StatusCodes} = require("http-status-codes");
+const CustomError = require("../errors");
 
 const register = async(req, res) =>{
-    res.send("register")
-}
+    const {email, name, password} = req.body;
+
+    const emailAreadyExist = await User.findOne({email});
+    if (emailAreadyExist){
+        throw new CustomError.BadRequestError("Email already exists")
+    }
+    // first registered user is an admin
+
+    const isFirstAccount = await User.countDocuments({}) === 0;
+    const role = isFirstAccount ? 'admin' : 'user';
+
+    const user = await User.create({email, name, password, role});
+
+    res.status(StatusCodes.CREATED).json({user});
+};
 
 const login = async(req, res) =>{
     res.send("login")
-}
+};
 
 const logout = async(req, res) =>{
     res.send("logout")
-}
+};
 
 module.exports = {
     register,
     login,
     logout,
-}
+};
