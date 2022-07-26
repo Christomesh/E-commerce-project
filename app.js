507fc8ed917e0f63d8ff24c04b850f86e204162b
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-26 11:02:34 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,48 +1,49 @@
-require("dotenv").config()
-require("express-async-errors")
+require("dotenv").config();
+require("express-async-errors");
 
 // express
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 // Other packages
-const morgan = require("morgan")
+const morgan = require("morgan");
 
 // database
-const connectDB = require("./db/connect")
+const connectDB = require("./db/connect");
 
 // routers
-const authRouter = require("./routes/authRoutes")
+const authRouter = require("./routes/authRoutes");
 
 //middleware
-const notFound = require("./middleware/not-found")
-const errorHandlerMiddleware = require("./middleware/error-handler")
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(morgan('tiny'))
-app.use(express.json())
+app.use(morgan('tiny'));
+app.use(express.json());
 
 app.get('/', (req, res)=>{
     res.send("E-commerce project")
-})
-app.use('/api/v1/auth', authRouter)
+});
+
+app.use('/api/v1/auth', authRouter);
 
 
 
 
 
-app.use(notFound)
-app.use(errorHandlerMiddleware)
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 // spin the server
 const start = async () =>{
     try {
-        await connectDB(process.env.MONGO_URL)
+        await connectDB(process.env.MONGO_URL);
         app.listen(port, ()=>
             console.log(`Server listening on port ${port}`)
-        )
+        );
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
-start()
+};
+start();
