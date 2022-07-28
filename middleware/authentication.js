71700358a9a59d9972c088d02c7a973a13b9231c
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-28 09:25:40 +0100

    added authentication and authorization

---
+++ middleware/authentication.js
@@ -0,0 +1,30 @@
+const CustomAPIError = require("../errors");
+const {isTokenValid} = require("../utils");
+
+const authenticateUser = async(req, res, next) =>{
+    const token = req.signedCookies.token;
+    if(!token){
+        throw new CustomAPIError.UnauthenticatedError("Authentication Invalid");
+    } 
+    try {
+        const {name, userId, role} = isTokenValid({token})
+        req.user = {name, userId, role}
+    } catch (error) {
+        throw new CustomAPIError.UnauthenticatedError("Authentication Invalid");
+    }
+    next()
+};
+
+const authorizePermissions = (...roles) =>{
+   return (req, res, next)=>{
+    if (!roles.includes(req.user.role)){
+        throw new CustomAPIError.UnauthorizedError("Not Authorized to acces this route")
+    };
+    next()
+   }
+}
+
+module.exports = {
+    authenticateUser,
+    authorizePermissions
+};
