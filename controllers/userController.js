411e1ef499ea5ef86db8eb909297d8dd84d44369
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-28 10:15:58 +0100

    finised showCurrentUser, UpdateUserPassword, UpdateUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User")
-const {CustomAPIError} = require("../errors")
+const CustomError = require("../errors")
 const {StatusCodes} = require("http-status-codes")
+const {createTokenUser, attachCookiesToResponse} = require("../utils")
 
 
 const getAllUsers = async (req, res) =>{
@@ -11,22 +12,48 @@ const getAllUsers = async (req, res) =>{
 const getSingleUser = async (req, res) => {
     const user = await User.findOne({_id: req.params.id}).select('-password')
     if (!user){
-        throw new CustomAPIError(`No user with id: ${req.params.id}`)
+        throw new CustomError.CustomAPIError(`No user with id: ${req.params.id}`)
     }
     res.status(StatusCodes.OK).json({user})
 }
 
 
 const showCurrentUser = async (req, res) =>{
-    res.send("Show cuurent user")
+    res.status(StatusCodes.OK).json({user: req.user})
 }
 
 const updateUser = async(req, res) =>{
-    res.send("Update User")
+    const {name, email} = req.body;
+    if (!name || !email){
+        throw new CustomError.BadRequestError("Please provide both values");
+    }
+    const user = await User.findOne({_id: req.usser.userId});
+    user.email = email;
+    user.name = name;
+    await user.save();
+
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({res, user:tokenUser});
+    res.status(StatusCodes.Ok).json({user: tokenUser});
+
+
 }
 
 const updateUserPassword = async (req, res)=>{
-    res.send("Update user password")
+    const {oldPassword, newPassword} = req.body;
+    if (!oldPassword || !newPassword){
+        throw new CustomError.BadRequestError("Please provide both values");
+    }
+    const user = await User.findOne({_id: req.user.userId});
+
+    isOldPasswordMatch = await user.comparePassword(oldPassword);
+    if (!isOldPasswordMatch){
+        throw new CustomError.UnauthenticatedError("Invalid credentials");
+    }
+    user.password = newPassword;
+    await user.save();
+
+    res.status(StatusCodes.OK).json({msg:"Success! Password updated!"});
 }
 
 module.exports = {
@@ -36,3 +63,19 @@ module.exports = {
     updateUser,
     updateUserPassword,
 }
+
+// Alternative update user with findOneAndUpdate
+// const updateUser = async (req, res) => {
+//   const { email, name } = req.body;
+//   if (!email || !name) {
+//     throw new CustomError.BadRequestError('Please provide all values');
+//   }
+//   const user = await User.findOneAndUpdate(
+//     { _id: req.user.userId },
+//     { email, name },
+//     { new: true, runValidators: true }
+//   );
+//   const tokenUser = createTokenUser(user);
+//   attachCookiesToResponse({ res, user: tokenUser });
+//   res.status(StatusCodes.OK).json({ user: tokenUser });
+// };
