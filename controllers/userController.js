9da74a97133f126a33256e7103a6ec7633301bb4
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-28 09:24:26 +0100

    updated changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,21 @@
+const User = require("../models/User")
+const {CustomAPIError} = require("../errors")
+const {StatusCodes} = require("http-status-codes")
+
 
 const getAllUsers = async (req, res) =>{
-    res.send("Get All users")
+    const users = await User.find({role:'user'}).select('-password')
+    res.status(StatusCodes.OK).json({users})
 }
 
 const getSingleUser = async (req, res) => {
-    res.send("Get Single User")
+    const user = await User.findOne({_id: req.params.id}).select('-password')
+    if (!user){
+        throw new CustomAPIError(`No user with id: ${req.params.id}`)
+    }
+    res.status(StatusCodes.OK).json({user})
 }
+
 
 const showCurrentUser = async (req, res) =>{
     res.send("Show cuurent user")
