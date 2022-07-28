0c40d353421dbafe36e6c17866690288ce0e8797
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-28 11:43:29 +0100

    added checkPermission.js

---
+++ utils/checkPermissions.js
@@ -0,0 +1,9 @@
+const CustomError = require("../errors");
+
+const checkPermissions = (requestUser, resourcesUserId) =>{
+    if (requestUser.role === 'admin') return;
+    if (requestUser.userId === resourcesUserId.toString()) return;
+    throw new CustomError.UnauthorizedError("Not authorized to acess the routes");
+};
+
+module.exports = checkPermissions;
