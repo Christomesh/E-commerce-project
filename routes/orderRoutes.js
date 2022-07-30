dadfbcb339d44a6e30482cb31bd8cd8aba9884b6
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-30 02:25:25 +0100

    fixed bugs

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -9,11 +9,13 @@ const {
     updateOrder,}  = require("../controllers/orderController");
 const { authenticateUser, authorizePermissions } = require("../middleware/authentication");
 
-
-router.get('/', authenticateUser, authorizePermissions('admin'), getAllOrders);
-router.get('/:id' , authenticateUser, getSingleOrder);
-router.get('/showAllMyOrders', authenticateUser, getCurrentUserOrders);
 router.post('/', authenticateUser, createOrder);
-router.patch('/:id', authenticateUser, updateOrder)
+router.get('/', authenticateUser, authorizePermissions('admin'), getAllOrders);
+
+router.get('/showAllMyOrders', authenticateUser, getCurrentUserOrders);
+// get single order must be place bellow cos showAllMyOrders will be treated as id
+router.get('/:id',authenticateUser, getSingleOrder);
+router.get('/:id',authenticateUser, getSingleOrder);
+router.patch('/:id', authenticateUser, updateOrder);
 
 module.exports = router;
